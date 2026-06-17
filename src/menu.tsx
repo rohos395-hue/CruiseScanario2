c5e38c58b9c2394ad949144df5e16e044b3dcaa5
rohos395-hue <rohos395@gmail.com>
2026-06-17 14:13:52 +0200

    Create menu.tsx

---
+++ src/menu.tsx
@@ -0,0 +1,27 @@
+import CruiseMap from "./assets/map_cata.png";
+
+<img
+    src={CruiseMap}
+    style={{
+        width: "100%",
+        maxWidth: "800px"
+    }}
+/>
+<div
+    style={{
+        position: "absolute",
+        left: "220px",
+        top: "100px",
+        width: "80px",
+        height: "60px",
+        cursor: "pointer",
+        backgroundColor: "rgba(255,0,0,0.2)"
+    }}
+    \***onClick={() => {
+
+        this.enterLocation(
+            "Bridge"
+        );
+
+    }}***/
+/>
