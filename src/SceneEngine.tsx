8066ee80f50ede37c4ac0058c0194d49dc7dd7fe
rohos395-hue <rohos395@gmail.com>
2026-07-07 22:21:29 +0200

    Update SceneEngine.tsx

--- src/SceneEngine.tsx
+++ src/SceneEngine.tsx
@@ -446,7 +446,7 @@ private evaluateConditionGroup(
             );
 
         });
-        state.log=state.log+"\n in validScenes "+validScenes;
+        state.log=state.log+"\n in validScenes "+JSON.stringify(validScenes);
 
     if (validScenes.length === 0) {
          state.log=state.log+"\n in validScenes is null";
