57b3b2a2e06d3b4040ee3ade64637e8b7744dbd8
rohos395-hue <rohos395@gmail.com>
2026-07-16 22:58:54 +0200

    Update SceneEngine.tsx

--- src/SceneEngine.tsx
+++ src/SceneEngine.tsx
@@ -91,7 +91,7 @@ Role:  ${character.role}
         //state.log=state.log+"newFrameIdInAppend: "+frame.id;
         //state.log += "newFrameIdInAppend: " + (frame?.id ?? "undefined");
         state.log += "messagesFrame: " + (state?.sceneState?.messagesFrame ?? "undefined");
-        if (state.sceneState.messagesFrame ===0&&frame.type ==="tabConversation2"){
+        if (state.sceneState.messagesFrame ===0&&frame.type ==="tabConversation"){
 
  let table = `
 <table>
@@ -109,7 +109,7 @@ Role:  ${character.role}
               return table  }
             //frame.afterPrompt}
 
-        else if  ((state.sceneState.messagesFrame ===0)&&((frame.type ==="emptyRoom")||(frame.type==="tabConversation"))){
+        else if  ((state.sceneState.messagesFrame ===0)&&((frame.type ==="emptyRoom")||(frame.type==="endAdd"))){
             return content+frame.afterPrompt}
         else { return ""}
         //<div style="text-align:center;">
